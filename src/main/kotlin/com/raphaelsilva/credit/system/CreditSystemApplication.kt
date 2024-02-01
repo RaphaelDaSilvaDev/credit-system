ffa792f5a2df03647b0ad285813f9ea5436b5724
RaphaelDaSilvaDev <raphael.h.silva@protonmail.com>
2024-01-31 21:59:14 -0300

    Setting JPA, Setting flyway, Create Customer, Credit entity, Create Migrations v1, v2

---
+++ src/main/kotlin/com/raphaelsilva/credit/system/CreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.raphaelsilva.credit.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditSystemApplication>(*args)
+}
---
+++ src/main/kotlin/com/raphaelsilva/credit/system/entity/Address.kt
@@ -0,0 +1,13 @@
+package com.raphaelsilva.credit.system.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Address (
+        @Column(nullable = false)
+        var zipCode: String = "",
+
+        @Column(nullable = false)
+        var street: String = ""
+)
